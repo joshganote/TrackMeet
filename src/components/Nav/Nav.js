f127715f7ae78f67864d5e59789ff1af4f7c19ad
Josh Ganote <j.ganote0825@gmail.com>
2020-01-16 16:34:17 -0600

    added path for profile

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -23,7 +23,7 @@ const Nav = (props) => (
           <Link className="nav-link" to="/info">
             Info Page
           </Link>
-          <Link className="nav-link" to="/features">
+          <Link className="nav-link" to="/home">
             Features
           </Link>
           <Link className="nav-link" to="/profile">
