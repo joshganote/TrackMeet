d26110e7da74440aea268c389967a9b9635e245e
Josh Ganote <j.ganote0825@gmail.com>
2020-01-17 10:50:36 -0600

    imported get router that was in user.router. I wanted to keep user.router just for login, and then have profile to handle all api calls for users profiles

---
+++ server/routes/profile.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+    const queryText = `SELECT * FROM "user"
+                      JOIN "user_roles" ON "user"."role_id" = "user_roles"."id"
+                      JOIN "portfolio" ON "user"."id" = "portfolio"."user_id"
+                      JOIN "profile" ON "user"."id" = "profile"."user_id"
+                      JOIN "project" ON "user"."id" = "project"."artist_id"
+                      WHERE "user"."id" = $1;`;
+    pool.query(queryText, [req.params.id])
+        .then((response) =>{
+          res.send(response.rows[0]);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(500);
+        })
+  })
+
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
