7b29e3c2bfe374f7e4167ab52490cc46489d3116
Josh Ganote <j.ganote0825@gmail.com>
2020-01-17 14:32:27 -0600

    imported component file that contains code for google map api

--- src/components/Features/Graphic/Graphic.js
+++ src/components/Features/Graphic/Graphic.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
+import GraphicMap from '../Graphic/GraphicMap';
 
 
 class Graphic extends Component {
@@ -12,6 +13,8 @@ class Graphic extends Component {
         return (
             <div>
                 <h2>{this.state.heading}</h2>
+                <p>once page loads it just shows "My Marker". Refreash page and it shows the outline of the map with error." </p>
+                <GraphicMap />
             </div>
         );
     }
--- src/components/Features/Video/Video.js
+++ src/components/Features/Video/Video.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
+import VideoMap from '../Video/VideoMap';
 
 class Video extends Component {
     state = {
@@ -11,6 +12,8 @@ class Video extends Component {
         return (
             <div>
                 <h2>{this.state.heading}</h2>
+                <p>once page loads it just shows "My Marker". Refreash page and it shows the outline of the map with error." </p>
+                <VideoMap />
             </div>
         );
     }
