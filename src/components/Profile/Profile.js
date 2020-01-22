bf90360ab006ed7069f335019931d04912f2eec1
Josh Ganote <j.ganote0825@gmail.com>
2020-01-22 10:46:49 -0600

    imported imageupload component and created a toggle to open and close upload box. in the css folder we set the size format for images

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import ImageUpload from '../ImageUpload/ImageUpload';
+import './Profile.css';
 
 class Profile extends Component {
     state = {
         heading: 'Profile',
         bioIsEditable: false,
+        bioIsCreated: false,
         bio: '',
         newBio: '',
+        selectedFiles: null, 
+        toggleUpload: false
     };
+
+    // AWS event handler
+    multipleFileChangedHandler = (event) => {
+        this.setState({
+         selectedFiles: event.target.files
+        });
+        console.log( event.target.files );
+       };
 
     componentDidMount() {
         this.props.dispatch({
             type: 'GET_PROFILE',
+            payload: this.props.store.user.id,
         })
     }
     handleChangeBio = (event) => {
         this.setState({
-            newBio: event.target.value
+            newBio: event.target.value,
+            bio: event.target.value
         })
     }
-    // input field to change bio isn't visible until 
-    // edit bio button is clicked
+
+    // input field to create bio isn't visible until create bio button is clicked
+    createBio = (event) => {
+        this.setState({
+            bio: this.state.bio,
+            bioIsCreated: true,
+        })
+    }
+    saveCreateBio = (event) => {
+        this.props.dispatch({
+            type: 'POST_PROFILE',
+            payload: {
+                bio: this.state.bio,
+            }
+        })
+        this.setState({
+            bio: '',
+            bioIsCreated: false,
+        })
+    }
+    // input field to change bio isn't visible until edit bio button is clicked
     editBio = (event) => {
         this.setState({
             newBio: this.state.bio,
@@ -29,25 +63,59 @@ class Profile extends Component {
         })
     };
 
-    saveBio = (event) => {
+    // saveBio = (event) => {
+    //     this.props.dispatch({
+    //         type: 'PUT_PROFILE',
+    //         payload: {
+    //             bio: this.state.newBio,
+    //             id: this.props.store.user.id,
+    //         }
+    //     });
+    //     // {
+    //     //     bio: string,
+    //         // id: integer,
+    //     // }
+    //     this.setState({
+    //         bio: '',
+    //         bioIsEditable: false,
+    //     })
+    // };
+
+    toggleUpload = event => {
         this.setState({
-            bio: this.state.newBio,
-            bioIsEditable: false,
+            toggleUpload: !this.state.toggleUpload
         })
-    };
+    }
 
     render() {
 
         return (
             <div>
                 <h2>{this.state.heading}</h2>
+                <div>
+                {this.props.store.user.profile_img && (<img src={this.props.store.user.profile_img}></img>)}
+                </div>
                 {this.props.store.user.username}
                 <br />
                 {this.props.store.user.city}
                 <br />
-                {this.props.store.profile.bio}
+                {this.props.store.user.bio}
                 <br />
-                {this.state.bioIsEditable ?
+                {this.state.bioIsCreated ?
+                    <input
+                        type="text"
+                        placeholder="Create Bio"
+                        value={this.state.bio}
+                        onChange={this.handleChangeBio}
+                    /> :
+                    <span>{this.state.bio}</span>
+                }
+                {this.state.bioIsCreated ?
+                    <button onClick={this.saveCreateBio}>Put  Bio</button> :
+                    <button onClick={this.createBio}>Create Bio</button>
+                }
+                <br />
+                {/* {this.state.bioIsEditable ?
                     <input
                         type="text"
                         placeholder="Edit Bio"
@@ -58,9 +126,12 @@ class Profile extends Component {
                 }
                 {this.state.bioIsEditable ?
                     <button onClick={this.saveBio}>Save Bio</button> :
-                    <button onClick={this.editBio}>Edit Bio</button>
-                }
-
+                    <button onClick={this.editBio}>Edit Bio</button>  
+                } */}
+                
+                { !this.state.toggleUpload && <button onClick={this.toggleUpload}>Upload Image</button>}
+                { this.state.toggleUpload && (<ImageUpload toggle={this.toggleUpload}/>)}
+                
             </div>
         );
     }
