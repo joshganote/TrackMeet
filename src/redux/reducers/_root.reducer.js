c068defb895dc160114ed8f88a2b06e87fe43f27
Josh Ganote <j.ganote0825@gmail.com>
2020-01-22 17:44:30 -0600

    imported reducers for all user roles, and then one for selected users

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -4,6 +4,7 @@ import loginMode from './loginMode.reducer';
 import user from './user.reducer';
 import profile from './profile.reducer';
 import user_role from './user_role.reducer';
+import selected_user from './selected_user.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   profile, // expecting get back data from user, user_roles, profile, and project table from database
   user_role, // contains user table data from all users with a specific user role
+  selected_user, // contains user table data for one specific user: id
 });
 
 export default rootReducer;
