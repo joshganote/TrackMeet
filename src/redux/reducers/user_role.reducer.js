ca63fbda940e2ef278acfc3319496641c65b9446
Josh Ganote <j.ganote0825@gmail.com>
2020-01-22 11:03:34 -0600

    made three reducers to take in all user data with a specific user role

---
+++ src/redux/reducers/user_role.reducer.js
@@ -0,0 +1,41 @@
+import { combineReducers } from 'redux';
+
+const producer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_PRODUCER':
+        return action.payload;
+      case 'UNSET_PRODUCER':
+        return {};
+      default:
+        return state;
+    }
+  };
+  
+  const graphic = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_GRAPHIC':
+        return action.payload;
+      case 'UNSET_GRAPHIC':
+        return {};
+      default:
+        return state;
+    }
+  };
+
+  const video = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_VIDEO':
+        return action.payload;
+      case 'UNSET_VIDEO':
+        return {};
+      default:
+        return state;
+    }
+  };
+
+  export default combineReducers({
+    producer,
+    graphic,
+    video,
+  });
+  
