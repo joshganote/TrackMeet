9cb4746590bf21ac50d4a2f55d67ecfb6a4f7bfe
Josh Ganote <j.ganote0825@gmail.com>
2020-01-22 10:51:59 -0600

    organizing file. commented out the sagas I wasn't using

--- src/redux/sagas/profile.saga.js
+++ src/redux/sagas/profile.saga.js
@@ -1,20 +1,64 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
+// This will GET all profile data for a specific user
 function* profileUser(action) {
-    const id = action.payload.id
-    try{
-        const response = yield axios.get('/api/profile' + id);
+    const id = action.payload;
+    try {
+        const response = yield axios.get('/api/profiles/' + id);
+        console.log(response);
         yield put({
             type: 'SET_PROFILE',
-            payload: response.data[0]
+            payload: {
+                ...response.data
+            }
         });
     } catch (err) {
         console.log('couldnt get profile', err);
     }
 }
+// This will UPDATE data entry for specific user
+function* updateUserBio(action) {    
+    try {
+//s3 call
+        const response = yield axios.put('/api/profiles/bio/', action.payload);
+        console.log(response);
+        yield put({
+            type: 'GET_PROFILE',
+        });
+    } catch (err) {
+        console.log('error profile PUT', err);
+    }
+}
+// This will send a POST to create a data entry for a specific user
+// function* createProfileEntry(action) {
+//     const id = action.payload.id;
+//     try {
+//         const response = yield axios.post('api/profiles/create-profile/' + id, action.payload);
+//         console.log(response);
+//         yield put({ type: 'POST_PROFILE', payload: id});
+//     } catch (err) {
+//         console.log('error profile POST', err);
+//     }
+// }
+// This will UPDATE data entry for specific user
+// function* editProfileTable(action) {
+//     const id = action.payload.id;
+//     console.log(action.payload);
+//     try {
+//         const response = yield axios.put('/api/profiles/profile/' + id, action.payload);
+//         console.log(response);
+//         yield put({
+//             type: 'GET_PROFILE',
+//             payload: id,
+//         });
+//     } catch (err) {
+//         console.log('error profile PUT', err);
+//     }
+// }
 function* profileSaga() {
     yield takeLatest('GET_PROFILE', profileUser);
-  }
-  
-  export default profileSaga;
+    yield takeLatest('POST_PROFILE', updateUserBio);
+    //yield takeLatest('PUT_PROFILE', editProfileTable);
+}
+export default profileSaga;
