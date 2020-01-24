a4d15908d88230c9fbfe870883321900e6a94040
Josh Ganote <j.ganote0825@gmail.com>
2020-01-24 13:44:20 -0600

    Tried making a post call to send a message to the data base. Imported the list items from the incoming messages and sent messages map function

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import mapStoreToProps from '../Contact/Contact';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import SentMessageList from '../Message/SentMessageList';
+import IncomingMessageList from '../Message/IncomingMessageList';
 
 class Messages extends Component {
     state = {
         heading: 'Send a Message',
+        newMessage: '',
     };
+    componentDidMount() {
+        this.props.dispatch({
+            type: 'GET_SENT_MESSAGES',
+            payload: this.props.store.user.id
+        })
+        this.props.dispatch({
+            type: 'GET_INCOMING_MESSAGES',
+            payload: this.props.store.user.id
+        })
+    }
+
+    handleChangeField = (event) => {
+        this.setState({
+            newMessage: event.target.value,
+        })
+    }
+
+    sendMessage = (event) => {
+        this.props.dispatch({
+            type: 'POST_MESSAGE',
+            payload: {
+                newMessage: this.state.newMessage,
+                // to_user_id:
+                // from_user_id:
+            }
+        })
+    }
 
     render() {
         return (
             <div>
                 <h2>{this.state.heading}</h2>
+                <IncomingMessageList incomingMessages={this.props.store.incomingMessages} />
+                <SentMessageList sentMessages={this.props.store.sentMessages} />
+                <input
+                    type="text"
+                    placeholder="Message"
+                    value={this.state.newMessage}
+                    onChange={this.handleChangeField}
+                />
+                <button onClick={this.sendMessage}>Send</button>
+
             </div>
         );
     }
