e24c4422134f6ca469b9e5c56be7299fef61cd0e
Josh Ganote <j.ganote0825@gmail.com>
2020-01-24 13:40:20 -0600

    made a variable to handel multiple lat/lng coordinates. mapped through them with an array

--- src/components/Features/Graphic/GraphicMap.js
+++ src/components/Features/Graphic/GraphicMap.js
@@ -1,35 +1,84 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+import '../MapStyles/Map.css';
+
+//const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const someCords = [
+  {
+    lat: 38.966547,
+    lng: -94.581934,
+    text: "Artisan Graphic Design"
+  },
+  {
+    lat: 38.8991893,
+    lng: -94.5828128,
+    text: "WebWorks of KC"
+  },
+  {
+    lat: 38.9205196,
+    lng: -94.438007,
+    text: "Willoughby Design"
+  }
+]
+
 class GraphicMap extends Component {
   static defaultProps = {
     center: {
       lat: 39.100105,
       lng: -94.5781416
     },
-    zoom: 11
+    zoom: 10
   };
- 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        clicked: false,
+        currentToastMessage: '',
+    }
+}
+clickMarker = (message) => (event) => {
+  this.setState({
+      clicked: !this.state.clicked,
+      currentToastMessage: message
+  })
+}
+
   render() {
+    let toast = <div></div>;
+
+    if(this.state.clicked) {
+        toast = (
+            <div className="scotts-toast">
+                {this.state.currentToastMessage}
+            </div>
+        )
+    }
+    const htmlArray = someCords.map((item, index) => {
+      return (
+        <div
+        className="test" onClick={this.clickMarker(item.clickMessage)}
+        lat={item.lat}
+        lng={item.lng}
+    >
+        {item.text}
+    </div>
+      )
+    })
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '50vh', width: '50%' }}>
+        <div style={{ height: '50vh', width: '80%' }}>
+          {toast}
         <GoogleMapReact
-         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS  }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
+          {htmlArray}
         </GoogleMapReact>
       </div>
     );
   }
 }
- 
+
 export default GraphicMap;
--- src/components/Features/Producer/ProducerMap.js
+++ src/components/Features/Producer/ProducerMap.js
@@ -1,35 +1,84 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+import '../MapStyles/Map.css';
+
+//const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const someCords = [
+  {
+    lat: 39.100105,
+    lng: -94.5781416,
+    text: "JTL Studio"
+  },
+  {
+    lat: 39.0985748,
+    lng: -94.5782673,
+    text: "Element Recording Studio"
+  },
+  {
+    lat: 38.9205482,
+    lng: -94.4380516,
+    text: "Cypher Sound Studios"
+  }
+]
+
 class ProducerMap extends Component {
   static defaultProps = {
     center: {
       lat: 39.100105,
       lng: -94.5781416
     },
-    zoom: 11
+    zoom: 10
   };
- 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        clicked: false,
+        currentToastMessage: '',
+    }
+}
+clickMarker = (message) => (event) => {
+  this.setState({
+      clicked: !this.state.clicked,
+      currentToastMessage: message
+  })
+}
+
   render() {
+    let toast = <div></div>;
+
+    if(this.state.clicked) {
+        toast = (
+            <div className="scotts-toast">
+                {this.state.currentToastMessage}
+            </div>
+        )
+    }
+    const htmlArray = someCords.map((item, index) => {
+      return (
+        <div
+        className="test" onClick={this.clickMarker(item.clickMessage)}
+        lat={item.lat}
+        lng={item.lng}
+    >
+        {item.text}
+    </div>
+      )
+    })
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '50vh', width: '50%' }}>
+        <div style={{ height: '50vh', width: '80%' }}>
+          {toast}
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS}}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
+          {htmlArray}
         </GoogleMapReact>
       </div>
     );
   }
-} 
- 
+}
+
 export default ProducerMap;
