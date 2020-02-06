188c0611fde719b04d139b23f285f4701f44b818
Josh Ganote <j.ganote0825@gmail.com>
2020-02-05 19:12:05 -0600

    took out the info link I wasn't using

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -21,11 +21,7 @@ const Nav = (props) => (
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.store.user.id && (
         <>
-          <Link className="nav-link" to="/info">
-            Info Page
-          </Link>
-           
-          <Link className="nav-link" to="/home">
+           <Link className="nav-link" to="/home">
             Features
           </Link>
           <Link className="nav-link" to="/profile">
